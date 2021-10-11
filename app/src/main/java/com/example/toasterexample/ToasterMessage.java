19ae0d77deadd60ae3a73402b1883f1c3a21e9b4
Phanitapu <venkateswarlu.phanitapu@concentrix.com>
2021-10-11 14:03:42 +0530

    Add all my files

---
+++ app/src/main/java/com/example/toasterexample/ToasterMessage.java
@@ -0,0 +1,10 @@
+package com.example.toasterexample;
+
+public class ToasterMessage {
+
+    public static void s(Context c,String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}

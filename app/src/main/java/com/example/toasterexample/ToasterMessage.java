01d3cba9d18ca19a40e682a6199ecf668b7ec6da
venkatp4 <79449355+venkatp4@users.noreply.github.com>
2021-10-11 14:17:13 +0530

    Update ToasterMessage.java

--- app/src/main/java/com/example/toasterexample/ToasterMessage.java
+++ app/src/main/java/com/example/toasterexample/ToasterMessage.java
@@ -1,8 +1,11 @@
 package com.example.toasterexample;
 
+import android.content.Context;
+import android.widget.Toast;
+
 public class ToasterMessage {
 
-    public static void s(Context c,String message){
+    public static void s(Context c, String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
